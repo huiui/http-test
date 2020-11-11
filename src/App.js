c4ab69489d81f901120206d3cda5a240d2155e54
Zihui Wang <zihui0714@gmail.com>
2020-11-10 18:05:43 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import tcb from "@cloudbase/js-sdk";
 
 function App() {
+  let url =
+    "https://test-cloudbase-4g2syy34ec7a6083.service.tcloudbase.com/hello";
+
+  const data = {
+    "a": 3,
+    "b": 4,
+  };
+
+
+  // const requestOptions = {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({ a: 3, b: 4 }),
+  // };
+
+  const clickHandle = () => {
+    // console.log("????");
+    axios
+      .post(url,data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        //console.log(err);
+      });
+
+    // const app = tcb.init({
+    //   env: "test-cloudbase-4g2syy34ec7a6083",
+    // });
+    // app
+    //   .callFunction({
+    //     // 云函数名称
+    //     name: "hello",
+    //     // 传给云函数的参数
+    //     data: {
+    //       a: 1,
+    //       b:2,
+    //     },
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err)=>{
+    //     console.log(err);
+    //   });
+    // fetch(url, requestOptions)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>for test only</p>
+      <button onClick={clickHandle}>click</button>
     </div>
   );
 }
